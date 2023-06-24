1306fd558d2cfbbf74f4b8875a608d22eae92cea
Kyle Harrity <hpkyh47@gmail.com>
2023-06-23 23:09:23 -0400

    add screenshot

--- index.ts
+++ index.ts
@@ -22,12 +22,12 @@ varying float v_Time;
 varying vec3 v_Color;
 
 vec3 palette(float t) {
-    vec3 a = vec3(0.5,0.5,0.5);
-    vec3 b = vec3(0.5,0.5,0.5);
-    vec3 c = vec3(1.0,1.0,1.0);
-    vec3 d = vec3(0.263,0.416,0.557);
-    
-    return a + b*cos(6.28318*(c*t*d) );
+	vec3 a = vec3(0.5,0.5,0.5);
+	vec3 b = vec3(0.5,0.5,0.5);
+	vec3 c = vec3(1.0,1.0,1.0);
+	vec3 d = vec3(0.263,0.416,0.557);
+
+	return a + b*cos(6.28318*(c*t*d) );
 }
 
 vec4 mainImage(vec2 fragCoord){
